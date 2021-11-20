ba639f46d2a508b9e6180976979e04b8ecad6f00
jayphbee <jayphhee@163.com>
2021-11-21 04:31:41 +0800

    add deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,20 +3,32 @@
 // When running the script with `hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
 import { ethers } from 'hardhat';
-import { Contract, ContractFactory } from 'ethers';
 
 async function main(): Promise<void> {
-  // Hardhat always runs the compile task when running scripts through it.
-  // If this runs in a standalone fashion you may want to call compile manually
-  // to make sure everything is compiled
-  // await run("compile");
-  // We get the contract to deploy
-  const TestTokenFactory: ContractFactory = await ethers.getContractFactory(
-    'TestToken',
-  );
-  const testToken: Contract = await TestTokenFactory.deploy();
-  await testToken.deployed();
-  console.log('TestToken deployed to: ', testToken.address);
+  const [deplyer, carRepairFeeWallet] = await ethers.getSigners();
+
+  const carcarTokenFactory = await ethers.getContractFactory("CarCarToken");
+  const carcarTokenContract = await carcarTokenFactory.deploy();
+  console.log("deploy CarCarToken at: ", carcarTokenContract.address);
+
+  const carcarNftFactory = await ethers.getContractFactory("CarCarNft");
+  const carcarNftContract = await carcarNftFactory.deploy();
+  console.log("deploy CarCarNft at: ", carcarNftContract.address);
+
+  const auctionMarketFactory = await ethers.getContractFactory("AuctionMarket");
+  const auctionMarketContract = await auctionMarketFactory.deploy(carcarTokenContract.address, carcarNftContract.address);
+  console.log("deploy auctionMarketContract at: ", auctionMarketContract.address);
+
+  const CarCarGameLogicFactory = await ethers.getContractFactory("CarCarGameLogic");
+  const carCarGameLogicContract = await CarCarGameLogicFactory.deploy(carcarNftContract.address, carcarTokenContract.address, carRepairFeeWallet.address, deplyer.address);
+  console.log("deploy CarCarGameLogic at: ", carCarGameLogicContract.address);
+
+  // set minter
+  await carcarNftContract.setMinter(carCarGameLogicContract.address);
+  console.log("set minter done...")
+  // approve CarCarGameLogic to opeerate all CarCarNft
+  await carcarNftContract.setApprovalForAll(carCarGameLogicContract.address, true);
+  console.log("approve nft done...")
 }
 
 // We recommend this pattern to be able to use async/await everywhere
